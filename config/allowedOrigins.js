e83a87e0f10f035aec8d852d3f847fd8edd462cf
FernandoDuffoo <fduffoo@aol.com>
2023-05-07 20:49:50 -0400

    update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'https://materialistic-dawn-angle.glitch.me',
+    'https://cyber-mango-harpymimus.glitch.me',
     'https://dazzling-snickerdoodle-777101.netlify.app',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
