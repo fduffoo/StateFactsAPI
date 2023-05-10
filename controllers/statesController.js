890d194a60953ef033a5dfe7b8a066d447287003
FernandoDuffoo <fduffoo@aol.com>
2023-05-10 14:54:00 -0400

    u

--- controllers/statesController.js
+++ controllers/statesController.js
@@ -71,7 +71,7 @@ const createFunfact = async (req, res) => {
         return res.status(400).json({'message': "State fun facts value must be an array"});
     }
      const code = req.params.state.toUpperCase();
-       if(!await State.findOneAndUpdate({statecode: State.statecode},{$push: {"funfacts": req.body.funfacts}})){
+       if(!await State.findOneAndUpdate({statecode: code},{$push: {"funfacts": req.body.funfacts}})){
             await State.create({ 
                 statecode: State.code,
                 funfacts: req.body.funfacts
