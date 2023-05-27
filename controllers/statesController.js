df7996d01c857a3ae10872d1a4f7599566e976f0
FernandoDuffoo <fduffoo@aol.com>
2023-05-27 10:16:24 -0400

    update

--- controllers/statesController.js
+++ controllers/statesController.js
@@ -4,180 +4,211 @@ const data = {
     states: require('../model/statesData.json'),
     setStates: function (data) { this.states = data }
 }
+/*
+const State = require('../model/State');
+const statesJSONData = require('../model/statesData.json');
+*/
 
-const getAllStates = (req, res) => {
-    res.json(data.states);
+const getAllStates = async (req, res) => {
+    const { contig } = req?.query // default is false 
+
+    let statesList = [...statesJSONData]; // all states
+
+    // filter if contig query parameter is true
+    if (contig === 'true') {
+        statesList = statesList.filter(state => state.code !== 'AK' && state.code !== 'HI')
+    }
+
+    // filter if contig query parameter is false
+    if (contig === 'false') {
+        statesList = statesList.filter(state => state.code === 'AK' || state.code === 'HI')
+    }
+
+    const mongoStates = await State.find(); // all states in MongoDB
+
+    statesList.forEach(state => {
+        const stateExists = mongoStates.find(st => st.stateCode === state.code);
+        if (stateExists) state.funfacts = [...stateExists.funfacts]
+    })
+
+    res.json(statesList);
 }
 
-const createNewState = async (req, res) => {
-    if (!req?.body?.code || !req?.body?.funfacts) {
-        return res.status(400).json({ 'message': 'StateCode and funfacts are required' });
+const getOneState = async (req, res) => {
+    const code = req.code // sent from middleware verifyState
+
+    // find specific state in JSON data 
+    const state = statesJSONData.find(state => state.code === code)
+
+    // find state in MongoDB collection
+    const savedState = await State.findOne({ stateCode: code }).exec();
+
+    if (savedState) { // add fun facts if they exist
+        state.funfacts = [...savedState.funfacts]
     }
 
-    try {
-        const result = await State.create({
-            code: req.body.code,
-            funfacts: req.body.funfacts
-        });
-
-        res.status(201).json(result);
-    } catch (err) {
-        console.error(err);
-    }
+    return res.json(state)
 }
 
-const updateState = async (req, res) => {
-    if (!req?.body?.code) {
-        return res.status(400).json({ 'message': 'State code parameter is required.' });
-    }
+const getRandomFact = async (req, res) => {
+    const code = req.code // sent from middleware verifyState
 
-    const state = await State.findOne({ _code: req.body.code }).exec();
-    if (!state) {
-        return res.status(204).json({ 'message': 'No state matches ID ${req.body.code}.' });
-    }
-    if (req.body?.code) state.code = req.body.code;
-    if (req.body?.funfacts) state.funfacts = req.body.funfacts;
-    const result = await state.save();
-    res.json(result);
+    // find specific state in JSON data 
+    const state = statesJSONData.find(state => state.code === code)
+
+    // find state in MongoDB collection
+    const savedState = await State.findOne({ stateCode: code }).exec();
+
+    // Checking if the savedState exists and if it does,  
+    // using Optional Chaining (?.) to confirm the savedState 
+    // has a funfacts array and the array is not empty
+    if (!savedState?.funfacts?.length) return res.status(404).json({ 'message': `No Fun Facts found for ${state.state}` })
+
+    // Get random element from funfacts array
+    const randomFact = savedState.funfacts[Math.floor(Math.random() * savedState.funfacts.length)];
+
+    return res.json({ 'funfact': randomFact })
+
 }
 
-const deleteState = async (req, res) => {
-    if (!req?.body?.code) return res.status(400).json({ 'message': 'State ID required.' });
+const addNewStateFacts = async (req, res) => {
+    const code = req.code;
+    const { funfacts } = req?.body; // destructure body data 
 
-    const state = await State.findOne({ _code: req.body.code }).exec();
-    if (!state) {
-        return res.status(204).json({ 'message': 'No state matches ID ${req.body.code}.' });
-    }
-    const result = await state.deleteOne();
-    res.json(result);
-}
+    if (!funfacts) return res.status(400).json({ 'message': 'State fun facts value required' })
 
-const getState = async (req, res) => {
-    const state = data.states.find(st => st.code === (req.params.state.toUpperCase()));
-    if (!state) {
-      return res.status(404).json({ 'message': 'Invalid state abbreviation parameter' });
-    }
-    res.json(state);
-}
+    if (!Array.isArray(funfacts)) return res.status(400).json({ 'message': 'State fun facts value must be an array' })
 
-const createFunfact = async (req, res) => {
-    if (!req?.params?.state){ 
-        return res.status(400).json({'message': 'Invalid state abbreviation parameter'});
-    }
-    if(!req?.body?.funfacts){
-        return res.status(400).json({"message": "State fun facts value required"});
-    }
-    if(!Array.isArray(req.body.funfacts)) {
-        return res.status(400).json({'message': "State fun facts value must be an array"});
-    }
-    const code = req.params.state.toUpperCase();
-    if(!await State.findOneAndUpdate({code: code},{$push: {"funfacts": req.body.funfacts}})){
-            await State.create({ 
-                code: code,
-                funfacts: req.body.funfacts
-             });
+    // find state in MongoDB collection
+    const savedState = await State.findOne({ stateCode: code }).exec();
+
+    let result;
+    if (savedState) { //update existing 
+        savedState.funfacts = [...savedState.funfacts, ...funfacts]
+        result = await savedState.save();
+        return res.json(result)
+    } else { //create new
+        try {
+            result = await State.create({
+                stateCode: code,
+                funfacts
+            })
+            return res.status(201).json(result)
+        } catch (err) {
+            console.error(err)
         }
-    const result = await State.findOne({code: code}).exec();
-    res.status(201).json(result);
+    }
+}
+
+const updateStateFact = async (req, res) => {
+    const code = req.code;
+    let { index, funfact } = req?.body; // destructure body data 
+
+    if (!index) return res.status(400).json({ 'message': 'State fun fact index value required' })
+
+    // adjust for 0 index of array
+    index = index - 1;
+
+    if (!funfact) return res.status(400).json({ 'message': 'State fun fact value required' })
+
+    // find specific state in JSON data 
+    const state = statesJSONData.find(state => state.code === code)
+
+    // find state in MongoDB collection
+    const savedState = await State.findOne({ stateCode: code }).exec();
+
+    // Checking if the savedState exists and if it does,  
+    // using Optional Chaining (?.) to confirm the savedState 
+    // has a funfacts array and the array is not empty
+    if (!savedState?.funfacts?.length) return res.status(404).json({ 'message': `No Fun Facts found for ${state.state}` })
+
+    if (!savedState.funfacts[index]) return res.status(404).json({ 'message': `No Fun Fact found at that index for ${state.state}` })
+
+    // Update funfact at specified index
+    savedState.funfacts[index] = funfact;
+    // Save state in MongoDB
+    const result = await savedState.save();
+    // Send response
+    res.json(result)
+}
+
+const deleteStateFact = async (req, res) => {
+    const code = req.code;
+    let { index } = req?.body; // destructure body data 
+
+    if (!index) return res.status(400).json({ 'message': 'State fun fact index value required' })
+
+    // adjust for 0 index of array
+    index = index - 1;
+
+    // find specific state in JSON data 
+    const state = statesJSONData.find(state => state.code === code)
+
+    // find state in MongoDB collection
+    const savedState = await State.findOne({ stateCode: code }).exec();
+
+    // Checking if the savedState exists and if it does,  
+    // using Optional Chaining (?.) to confirm the savedState 
+    // has a funfacts array and the array is not empty
+    if (!savedState?.funfacts?.length) return res.status(404).json({ 'message': `No Fun Facts found for ${state.state}` })
+
+    if (!savedState.funfacts[index]) return res.status(404).json({ 'message': `No Fun Fact found at that index for ${state.state}` })
+
+    // create new array by filtering out record to delete
+    const newFactsArray = savedState.funfacts.filter((ff, i) => i !== index)
+    // set funfacts to new array
+    savedState.funfacts = newFactsArray;
+    // save state to MongoDB
+    const result = await savedState.save();
+    // send response
+    res.json(result)
 }
 
 const getCapital = (req, res) => {
-    const state = data.states.find(st => st.code === (req.params.state.toUpperCase()));
-    if (!state) {
-        return res.status(404).json({ 'message': 'Invalid state abbreviation parameter' });
-    }
-    res.json({
-        "state": state.state,
-        "capital": state.capital_city
-    });
+    const code = req.code // sent from middleware verifyState
+
+    // find specific state in JSON data 
+    const state = statesJSONData.find(state => state.code === code)
+
+    res.json({ 'state': state.state, 'capital': state.capital_city })
 }
 
-const getNickname = (req, res) => {
-    const state = data.states.find(st => st.code === (req.params.state.toUpperCase()));
-    if (!state) {
-        return res.status(404).json({ 'message': 'Invalid state abbreviation parameter' });
-    }
-    res.json({
-        "state": state.state,
-        "nickname": state.nickname
-    });
+const getNickName = (req, res) => {
+    const code = req.code // sent from middleware verifyState
+
+    // find specific state in JSON data 
+    const state = statesJSONData.find(state => state.code === code)
+
+    res.json({ 'state': state.state, 'nickname': state.nickname })
 }
 
-const getPopulation = (req, res) => {
-    const state = data.states.find(st => st.code === (req.params.state.toUpperCase()));
-    if (!state) {
-        return res.status(404).json({ 'message': 'Invalid state abbreviation parameter' });
-    }
-    res.json({
-        "state": state.state,
-        "population": state.population.toLocaleString()
-    });
+const getPop = (req, res) => {
+    const code = req.code // sent from middleware verifyState
+
+    // find specific state in JSON data 
+    const state = statesJSONData.find(state => state.code === code)
+
+    res.json({ 'state': state.state, 'population': state.population.toLocaleString('en-US') })
 }
 
 const getAdmission = (req, res) => {
-    const state = data.states.find(st => st.code === (req.params.state.toUpperCase()));
-    if (!state) {
-        return res.status(404).json({ 'message': 'Invalid state abbreviation parameter' });
-    }
-    res.json({
-        "state": state.state,
-        "admitted": state.admission_date
-    });
-}
+    const code = req.code // sent from middleware verifyState
 
-const deleteFunfact = async (req, res) => {
-    const state = await State.findOne({ code: req.params.state });
-    if(!req.body.index) {
-        return res.status(400).json({ 'message': 'State fun fact index value required'});
-    }
-    if (!state) {
-        return res.status(404).json({ 'message': 'Invalid state abbreviation parameter' });
-    }
-        else if (!state.funfacts.length) {
-            res.json({ 'message': 'No Fun Facts found for ' + state.state })
-        }
-        else if (req.body.index <= 0 || req.body.index > state.funfacts.length) {
-            res.json({ 'message': 'No Fun Fact found at that index for ' + state.state })
-        }
-        const toEdit = state.funfacts;
-        toEdit.splice(req.body.index - 1, 1);
-        state.funfacts = toEdit;
-        const response = state.save()
-        res.json(response);
-}
+    // find specific state in JSON data 
+    const state = statesJSONData.find(state => state.code === code)
 
-const patchFunfact = async (req, res) => {
-    const state = await State.findOne({ code: req.params.state });
-    if(!req.body.index) {
-        return res.status(400).json({ 'message': 'State fun fact index value required'});
-    }
-    if (!state) {
-        return res.status(404).json({ 'message': 'Invalid state abbreviation parameter' });
-    }
-        else if (!state.funfacts.length) {
-            res.json({ 'message': 'No Fun Facts found for ' + state.state })
-        }
-        else if (req.body.index <= 0 || req.body.index > state.funfacts.length) {
-            res.json({ 'message': 'No Fun Fact found at that index for ' + state.state })
-        }
-        const toEdit = state.funfacts;
-        toEdit.splice(req.body.index - 1, 1);
-        state.funfacts = toEdit;
-        const response = state.save()
-        res.json(response);
+    res.json({ 'state': state.state, 'admitted': state.admission_date })
 }
 
 module.exports = {
     getAllStates,
-    createNewState,
-    updateState,
-    deleteState,
-    getState,
-    createFunfact,
+    getOneState,
+    getRandomFact,
+    addNewStateFacts,
+    updateStateFact,
+    deleteStateFact,
     getCapital,
-    getNickname,
-    getPopulation,
-    getAdmission,
-    deleteFunfact,
-    patchFunfact
+    getNickName,
+    getPop,
+    getAdmission
 }
--- routes/api/states.js
+++ routes/api/states.js
@@ -1,31 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const statesController = require('../../controllers/statesController');
+const verifyState = require('../../middleware/verifyState');
 
 router.route('/')
-    .get(statesController.getAllStates)
-    .post(statesController.createNewState)
-    .put(statesController.updateState)
-    .delete(statesController.deleteState);
+    .get(statesController.getAllStates) // returns html (if accepted) or text response with directions
 
 router.route('/:state')
-    .get(statesController.getState);
+    .get(verifyState(), statesController.getOneState) // retrieve all info about specified state
 
 router.route('/:state/funfact')
-    .post(statesController.createFunfact)
-    .delete(statesController.deleteFunfact)
-    .patch(statesController.patchFunfact);
+    .get(verifyState(), statesController.getRandomFact) // returns 1 random fun fact about the specified state 
+    .post(verifyState(), statesController.addNewStateFacts) // post 1 or more new state facts 
+    .patch(verifyState(), statesController.updateStateFact) // update existing fun fact at index
+    .delete(verifyState(), statesController.deleteStateFact) // delete fun fact at index 
 
 router.route('/:state/capital')
-    .get(statesController.getCapital);
+    .get(verifyState(), statesController.getCapital) // returns the state and capital
 
 router.route('/:state/nickname')
-    .get(statesController.getNickname);
+    .get(verifyState(), statesController.getNickName) // returns the state and nickname
 
 router.route('/:state/population')
-    .get(statesController.getPopulation);
+    .get(verifyState(), statesController.getPop) // returns the state and population
 
 router.route('/:state/admission')
-    .get(statesController.getAdmission);
+    .get(verifyState(), statesController.getAdmission) // returns the state and the date the state was admitted 
 
 module.exports = router;
