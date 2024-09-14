7db71ebb355bce86b0e5083c56bc6063e28ed7b9
Fernando Duffoo <fernandojduffoo@gmail.com>
2024-09-13 21:32:44 -0400

    Fixed issue

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -1,16 +1,11 @@
-module.exports = credentials
-
 const allowedOrigins = require('../config/allowedOrigins');
 
 const credentials = (req, res, next) => {
     const origin = req.headers.origin;
-
-    if (origin && allowedOrigins.includes(origin)) {
-        res.header('Access-Control-Allow-Credentials', 'true');
-        res.header('Access-Control-Allow-Origin', origin); // Set the allowed origin header
+    if (allowedOrigins.includes(origin)) {
+        res.header('Access-Control-Allow-Credentials', true);
     }
-
     next();
-}
+};
 
 module.exports = credentials;
