431ab9e55d4f16ab82543d52b3bc160076a8de58
Fernando Duffoo <fernandojduffoo@gmail.com>
2024-09-13 21:19:18 -0400

    Enhance CORS credentials middleware to handle origin header and set allowed origin

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -1,11 +1,16 @@
+module.exports = credentials
+
 const allowedOrigins = require('../config/allowedOrigins');
 
 const credentials = (req, res, next) => {
     const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.header('Access-Control-Allow-Credentials', true);
+
+    if (origin && allowedOrigins.includes(origin)) {
+        res.header('Access-Control-Allow-Credentials', 'true');
+        res.header('Access-Control-Allow-Origin', origin); // Set the allowed origin header
     }
+
     next();
 }
 
-module.exports = credentials
+module.exports = credentials;
