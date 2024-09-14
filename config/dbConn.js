db018a4d351911b0f99f63fe44066b2de08b33e3
Fernando Duffoo <fernandojduffoo@gmail.com>
2024-09-13 20:58:12 -0400

    Improve MongoDB connection handling and logging

--- config/dbConn.js
+++ config/dbConn.js
@@ -2,13 +2,12 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URI, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true
-        });
+        await mongoose.connect(process.env.DATABASE_URI);
+        console.log('Connected to MongoDB');
     } catch (err) {
-        console.error(err);
+        console.error('MongoDB connection failed:', err);
+        process.exit(1); // Exit the process with failure
     }
 }
 
-module.exports = connectDB
+module.exports = connectDB;
