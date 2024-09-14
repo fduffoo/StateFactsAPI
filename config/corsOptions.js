663ce60f886d9c69a26c5a97f78625be27609923
Fernando Duffoo <fernandojduffoo@gmail.com>
2024-09-13 21:07:21 -0400

    Refactor CORS options for readability and improved error handling

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,10 +2,10 @@ const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
         } else {
-            callback(new Error('Not allowed by CORS'));
+            callback(new Error(`Origin ${origin} is not allowed by CORS`));
         }
     },
     optionsSuccessStatus: 200
